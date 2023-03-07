4479b047f01e6bf8ea3662cc11d019e2b3e55bb3
pmakin-png <54557724+makin-stuff@users.noreply.github.com>
2023-03-07 17:15:42 -0500

    Whoops

--- Connected_Devices/Device_Data_Dashboard/MQTTRGBOLED/arduino_secrets.h
+++ Connected_Devices/Device_Data_Dashboard/MQTTRGBOLED/arduino_secrets.h
@@ -1,6 +1,4 @@
-#define SECRET_SSID "sandbox370"
-#define SECRET_PASS "+s0a+s03!2gether?"
-//#define SECRET_SSID "F65316"
-//#define SECRET_PASS "03974384"
+#define SECRET_SSID ""
+#define SECRET_PASS ""
 #define SECRET_MQTT_USER "public" // broker username
 #define SECRET_MQTT_PASS "public" // broker password
