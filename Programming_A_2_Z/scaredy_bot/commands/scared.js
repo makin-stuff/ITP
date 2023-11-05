24124e0b7260e65a90c2e4df132470696591cc21
pmakin-png <54557724+makin-stuff@users.noreply.github.com>
2023-11-04 21:21:49 -0400

    Remove

--- Programming_A_2_Z/scaredy_bot/commands/scared.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-
-export const data = new SlashCommandBuilder()
-  .setName("scared")
-  .setDescription("Replies to scared");
-
-export async function execute(interaction) {
-  await interaction.reply("I'm scared!");
-}
