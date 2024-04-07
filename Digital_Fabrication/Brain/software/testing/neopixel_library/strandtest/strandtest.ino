269d70b6eab4da2ec2367b5cc624c4f3018d781c
pmakin-png <54557724+makin-stuff@users.noreply.github.com>
2024-04-07 17:56:15 -0400

    reorganize software

--- Digital_Fabrication/Brain/software/testing/neopixel_library/strandtest/strandtest.ino
+++ Digital_Fabrication/Brain/software/testing/neopixel_library/strandtest/strandtest.ino
@@ -20,7 +20,7 @@
 #define LED_PIN    6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 80
+#define LED_COUNT 160
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -46,7 +46,7 @@ void setup() {
 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.setBrightness(10); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
 
--- Digital_Fabrication/Brain/software/testing/neopixel_library/strandtest2/strandtest2.ino
+++ Digital_Fabrication/Brain/software/testing/neopixel_library/strandtest2/strandtest2.ino
@@ -46,7 +46,7 @@ void setup() {
 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.setBrightness(20); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
 
