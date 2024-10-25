bd6ac1e97997c0a46dfe2385583c734cd5ed6a36
pmakin-png <54557724+makin-stuff@users.noreply.github.com>
2024-10-25 13:28:25 -0700

    Push most recent interface code

--- interFACE/code/01_working_code/sample_sequencer/sample_sequencer.ino
+++ interFACE/code/01_working_code/sample_sequencer/sample_sequencer.ino
@@ -15,20 +15,38 @@
 #include "AudioSampleSnot.h"
 #include "AudioSampleSwallow.h"
 #include "AudioSampleLaugh.h"
+#include "AudioSampleJosh.h"
 
 // GUItool: begin automatically generated code
-AudioPlayMemory          sample1;        //xy=134,256
-AudioPlayMemory          sample0;        //xy=136,212
-AudioPlayMemory          sample2;        //xy=136,299
-AudioPlayMemory          sample3;        //xy=136,341
-AudioPlayMemory          sample4;       //xy=138,489
-AudioPlayMemory          sample5;       //xy=140,530
-AudioPlayMemory          sample6;       //xy=141,570
-AudioPlayMemory          sample7;       //xy=142,613
-AudioMixer4              mixer;          //xy=310,292
-AudioMixer4              mixer1;         //xy=319,543
-AudioMixer4              mixer2;         //xy=508,386
-AudioOutputI2S           i2s1;           //xy=655,387
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioPlayMemory          sample1;        //xy=230,341
+AudioPlayMemory          sample0;        //xy=232,297
+AudioPlayMemory          sample2;        //xy=232,384
+AudioPlayMemory          sample3;        //xy=232,426
+AudioPlayMemory          sample4;        //xy=234,574
+AudioPlayMemory          sample5;        //xy=236,615
+AudioPlayMemory          sample6;        //xy=237,655
+AudioPlayMemory          sample7;        //xy=238,698
+AudioPlayMemory          sample8;       //xy=287,792
+AudioPlayMemory          sample9;       //xy=313,832
+AudioMixer4              mixer;          //xy=406,377
+AudioMixer4              mixer1;         //xy=415,628
+AudioMixer4              mixer3;         //xy=461,801
+AudioMixer4              mixer2;         //xy=604,471
+AudioOutputI2S           i2s1;           //xy=751,472
 AudioConnection          patchCord1(sample1, 0, mixer, 1);
 AudioConnection          patchCord2(sample0, 0, mixer, 0);
 AudioConnection          patchCord3(sample2, 0, mixer, 2);
@@ -37,33 +55,43 @@ AudioConnection          patchCord5(sample4, 0, mixer1, 0);
 AudioConnection          patchCord6(sample5, 0, mixer1, 1);
 AudioConnection          patchCord7(sample6, 0, mixer1, 2);
 AudioConnection          patchCord8(sample7, 0, mixer1, 3);
-AudioConnection          patchCord9(mixer, 0, mixer2, 0);
-AudioConnection          patchCord10(mixer1, 0, mixer2, 1);
-AudioConnection          patchCord11(mixer2, 0, i2s1, 0);
-AudioConnection          patchCord12(mixer2, 0, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=745,485
-AudioPlayMemory         sample8;
+AudioConnection          patchCord9(sample8, 0, mixer3, 0);
+AudioConnection          patchCord10(sample9, 0, mixer3, 1);
+AudioConnection          patchCord11(mixer, 0, mixer2, 0);
+AudioConnection          patchCord12(mixer1, 0, mixer2, 1);
+AudioConnection          patchCord13(mixer3, 0, mixer2, 2);
+AudioConnection          patchCord14(mixer2, 0, i2s1, 0);
+AudioConnection          patchCord15(mixer2, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=841,570
 // GUItool: end automatically generated code
 
 int noteCount = 0;
 
 // LEDs
-int step0 = 33;
+int step0 = 36;
 int step1 = 34;
 int step2 = 35;
-int step3 = 36;
+int step3 = 33;
 int step4 = 37;
 int step5 = 32;
 int step6 = 31;
 int step7 = 30;
 
-int brainTrig = 26; // Red button
+int tempo = 500;
+float volumeOut = 0.8; 
+
+int brainTrig = 26; // Nostril input!!!
 int brainState = 0;
 int lastBrainState = 0;
 
-int laughTrig = 28; // Yellow button
-int laughState = 0;
-int lastLaughState = 0;
+// buttons
+Bounce modeButton = Bounce(29, 15); // Red
+Bounce laughButton = Bounce(28, 15);  // Yellow
+int currentMode = 0;
+#define CLOCK 1
+#define BRAIN 2
+
+long randNumber;
 
 void setup() {
   // Step indicator LEDs
@@ -76,14 +104,18 @@ void setup() {
   pinMode(step6, OUTPUT);
   pinMode(step7, OUTPUT);
 
-  // Buttons!
+  // Brain trigger
   pinMode(brainTrig, INPUT);
-  pinMode(laughTrig, INPUT);
 
-  Serial.begin(115200);
+  // Buttons  
+  pinMode(29, INPUT); // Red
+  pinMode(28, INPUT); // Yellow
+  currentMode = CLOCK;
+
+  Serial.begin(9600);
   AudioMemory(200);
   sgtl5000_1.enable();
-  sgtl5000_1.volume(.8);
+  sgtl5000_1.volume(volumeOut);
 
   mixer.gain(1, 1);
   mixer.gain(2, 1);
@@ -94,54 +126,79 @@ void setup() {
   mixer1.gain(2, 1);
   mixer1.gain(3, 1);
   mixer1.gain(4, 1);
+
+  mixer3.gain(1, 1);
+  mixer3.gain(2, 1);
+  mixer3.gain(3, 1);
+  mixer3.gain(4, 1);
+
+  randomSeed(analogRead(A12));
 }
 
 void loop() {
 
+  modeButton.update();
+  laughButton.update();
+  
   // check if the tempo has changed
-//  tempo = map(analogRead(A13), 0, 1023, 200, 1500);
+  tempo = map(analogRead(A13), 0, 1023, 200, 1500);
+  
+  // check if the volume has changed
+  volumeOut = map(analogRead(A11), 0, 1023, 10, 0);
+  volumeOut = volumeOut/10;
+  sgtl5000_1.volume(volumeOut);
 
-//  // Check the laugh button
-//  laughState = digitalRead(laughTrig);
-//  if (laughState != lastLaughState) {
-//    if (laughState == HIGH) {
-//      sample8.play(AudioSampleLaugh);
-//    }
-//  }
-//  lastLaughState = laughState;
-    
-  brainState = digitalRead(brainTrig);
-
-  if (brainState != lastBrainState) {
-    if (brainState == HIGH) {
-//      Serial.println("step");
-      playSequence(noteCount);
-
-      if (noteCount < 7) {
-        noteCount++;
-      } else {
-        noteCount = 0;  // Reset count
-      }
-
-//      Serial.println(noteCount);
+  // Check to see if someone pressed yellow button
+  if (laughButton.fallingEdge()) {
+//    Serial.println("laugh");
+    randNumber = random(300);
+    Serial.println(randNumber);
+    if (randNumber < 250) {
+      sample8.play(AudioSampleLaugh);
+    } else {
+      sample9.play(AudioSampleJosh);
+    }
+  }
+  
+  // Switch the mode based on the button press
+  if (modeButton.fallingEdge()) {
+    switch (currentMode) {
+      case CLOCK:
+        currentMode = BRAIN;
+        Serial.println("Brain mode");
+        break;
+      case BRAIN:
+        currentMode = CLOCK;
+        Serial.println("Clock mode");
+        break;
     }
   }
 
-  // Update last brain state
-  lastBrainState = brainState;
+  if (currentMode == BRAIN) {
+//    analogWrite(A6, 255);
+    brainState = digitalRead(brainTrig);
+    
+    if (brainState != lastBrainState) {
+      if (brainState == HIGH) {
+        playSequence(noteCount, tempo, currentMode);
+        nextNote();
+      }
+    }
+    lastBrainState = brainState;
+  } else if (currentMode == CLOCK) {
 
+//    analogWrite(A5, 255);
+    playSequence(noteCount, tempo, currentMode);
+    nextNote();
+  }
 //  Serial.println(noteCount);
-}
+} 
 
 
 //This function plays a random sequence as set by a hardcoded scale
 //and calculation by a random value. The knobs determine note length,
 //filter attack time, envelope release time, and tempo via a simple delay.
-void playSequence(int noteCount) {
-//  Serial.println("step");
-  Serial.print("noteCount: ");
-  Serial.println(noteCount);
-  
+void playSequence(int noteCount, int tempo, int playMode) {
   int sampleNum;
 
   // For every note (step), read a different potentiometer
@@ -218,9 +275,23 @@ void playSequence(int noteCount) {
       break;
   }
 
+  // Control the speed of the sequence
+  if (playMode == BRAIN) {
+    delay(10);
+  } else if (playMode == CLOCK) {
+    delay(tempo); // Tempo comes from a potentiometer
+  }
+
   // Turn off indicator LEDs
-  delay(10);
   turnOffLEDs();
+}
+
+void nextNote() {
+  if (noteCount < 7) {
+    noteCount++;
+  } else {
+    noteCount = 0;  // Reset count
+  }
 }
 
 void turnOffLEDs() {
